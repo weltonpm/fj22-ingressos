f28103c36733cec8a549b8aac900308f89d590cc
welton <weltonpm16@gmail.com>
2017-10-21 17:14:40 -0200

    Implementando a tela de checkout.

--- src/main/java/br/com/caelum/ingresso/controller/CompraController.java
+++ src/main/java/br/com/caelum/ingresso/controller/CompraController.java
@@ -2,6 +2,7 @@ package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,15 +14,23 @@ import br.com.caelum.ingresso.model.form.CarrinhoForm;
 @Controller
 public class CompraController {
 	@Autowired
-	private	SessaoDao sessaoDao;
+	private SessaoDao sessaoDao;
 	@Autowired
-	private	LugarDao lugarDao;
+	private LugarDao lugarDao;
 	@Autowired
-	private	Carrinho carrinho;
+	private Carrinho carrinho;
+
 	@PostMapping("/compra/ingressos")
-	public	ModelAndView	enviarParaPagamento(CarrinhoForm	carrinhoForm){
-		ModelAndView modelAndView =	new	ModelAndView("redirect:/compra");
-		carrinhoForm.toIngressos(sessaoDao,	lugarDao).forEach(carrinho::add);
-		return	modelAndView;
+	public ModelAndView enviarParaPagamento(CarrinhoForm carrinhoForm) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/compra");
+		carrinhoForm.toIngressos(sessaoDao, lugarDao).forEach(carrinho::add);
+		return modelAndView;
+	}
+
+	@GetMapping("/compra")
+	public ModelAndView checkout() {
+		ModelAndView modelAndView = new ModelAndView("compra/pagamento");
+		modelAndView.addObject("carrinho", carrinho);
+		return modelAndView;
 	}
 }
--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,4 +28,8 @@ public class Carrinho {
 	public boolean isSelecionado(Lugar lugar) {
 		return ingressos.stream().map(Ingresso::getLugar).anyMatch(l -> l.equals(lugar));
 	}
+
+	public BigDecimal getTotal(){
+		return	ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+}
 }
