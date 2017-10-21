b790766dd7977eef0b862dca98012cf2e2407d9a
welton <weltonpm16@gmail.com>
2017-10-21 11:47:56 -0200

    Consumindo serviço para detalhes do filme.

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.ImdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -33,6 +35,9 @@ public class FilmeController {
     
     @Autowired
     private SessaoDao sessaoDao;
+    
+    @Autowired
+    private ImdbClient client;
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
@@ -99,7 +104,10 @@ public class FilmeController {
     	Filme filme = filmeDao.findOne(id);
     	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
     	
+    	Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+    	
     	modelAndView.addObject("sessoes", sessoes);
+    	modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
     	
     	return modelAndView;
     }
