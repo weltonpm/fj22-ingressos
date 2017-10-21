6fe328287b9c1e8e2cd1cd1cc3b51027b120382f
welton <weltonpm16@gmail.com>
2017-10-21 09:51:06 -0200

    Criando o catélogo de filmes e tela de detalhes do filme com sessões para compra.

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -25,5 +26,10 @@ public class SessaoDao {
 								   Sessao.class)
 				.setParameter("sala", sala)
 				.getResultList();
+	}
+	
+	public List<Sessao> buscaSessoesDoFilme(Filme filme) {
+		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
+				.setParameter("filme", filme).getResultList();
 	}
 }
